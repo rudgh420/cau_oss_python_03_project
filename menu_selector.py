671c428746a1b1e7638ccab5968f53a771b2e161
rudgh420 <abc40abc40@naver.com>
2023-06-11 15:29:05 +0900

    version#3

--- menu_selector.py
+++ menu_selector.py
@@ -22,29 +22,25 @@ def start_process(path):
             print("[4] ptype")
             print("[5] location")
             select = int(input('type:'))
-            if select == 1:
-                keyword = input('type name:')
-                print("not implemented yet")
-                # fill this block
+            if select == 1:                      # spots 와 keyword를  매개변수로 받는 filter_by_ 함수들 호출 
+                keyword = input('type name:')    # 각 함수들은 기존에 생성된 객체 삭제 후 새 list 저장
+                spots = parking_spot_manager.filter_by_name(spots, keyword)            
             elif select == 2:
                 keyword = input('type city:')
-                print("not implemented yet")
-                # fill this block
+                spots = parking_spot_manager.filter_by_city(spots, keyword)
             elif select == 3:
                 keyword = input('type district:')
-                print("not implemented yet")
-                # fill this block
+                spots = parking_spot_manager.filter_by_district(spots, keyword)
             elif select == 4:
                 keyword = input('type ptype:')
-                print("not implemented yet")
-                # fill this block
+                spots = parking_spot_manager.filter_by_ptype(spots, keyword)
             elif select == 5:
                 min_lat = float(input('type min lat:'))
-                max_lat = float(input('type max lat:'))
-                min_lon = float(input('type min long:'))
+                max_lat = float(input('type max lat:'))             #위의 함수들과는 다르게 filter_by_location 함수는  
+                min_lon = float(input('type min long:'))            #4개의 실수를 keyword 값으로 받는다.
                 max_lon = float(input('type max long:'))
-                print("not implemented yet")
-                # fill this block
+                keyword = (min_lat, max_lat, min_lon, max_lon)      
+                spots = parking_spot_manager.filter_by_location(spots, keyword)
             else:
                 print("invalid input")
         elif select == 3:
--- parking_spot_manager.py
+++ parking_spot_manager.py
@@ -34,14 +34,43 @@ def print_spots(spots):
         print(list)         #list 길이 출력 후 객체 list 하나씩 출력
 
 
+
+# spots 과 키워드를 매개변수로 받는 필터링 함수.
+# list 함축 사용하여 spots의 객체 중 키워드와 일치하는 값이 존재할 경우 result list에 저장
+
+def filter_by_name(spots, name):
+    result = [data for data in spots if name in data.get('name')]
+    return result
+
+def filter_by_city(spots, city):
+    result = [data for data in spots if city in data.get('city')]
+    return result
+
+def filter_by_district(spots, district):
+    result = [data for data in spots if district in data.get('district')]
+    return result
+
+def filter_by_ptype(spots, ptype):
+    result = [data for data in spots if ptype in data.get('ptype')]
+    return result
+
+
+def filter_by_location(spots, locations):
+    # 일치하는지 판단하는 위 함수들과 다르게 범위 안에 값이 있는지 판단하는 필터링 함수.
+    
+    min_lat, max_lat, min_long, max_long = locations # menu_slelctor 에서 keyword 로 4가지 범위값을 입력받음
+    result = [data for data in spots if min_lat < float(data.get('latitude')) < max_lat and min_long < float(data.get('longitude')) < max_long] 
+    return result         # string 인 data.get() 값을 실수형으로 바꿔줌.
+
+
 # 각 단계별로 테스트 (테스트할때 주석해제 후 사용)
 if __name__ == '__main__':
     print("Testing the module...")
     # version#2
-    import file_manager
-    str_list = file_manager.read_file("./input/free_parking_spot_seoul.csv")
-    spots = str_list_to_class_list(str_list)
-    print_spots(spots)
+    # import file_manager
+    # str_list = file_manager.read_file("./input/free_parking_spot_seoul.csv")
+    # spots = str_list_to_class_list(str_list)
+    # print_spots(spots)
 
     # version#3
     # spots = filter_by_district(spots, '동작')
